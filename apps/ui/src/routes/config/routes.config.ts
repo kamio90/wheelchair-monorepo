bf07c95e4669e9119e73bdafdc515813858a0bbc
kamio90 <kmusial40@gmail.com>
2023-04-22 14:30:13 +0200

    setup react router & clean up base create-react-app template

---
+++ apps/ui/src/routes/config/routes.config.ts
@@ -0,0 +1,10 @@
+import { Home } from '../lazy-config/home-rotue-lazy-config.route';
+
+const routesConfig = {
+  Home: {
+    path: '/',
+    component: Home,
+  },
+};
+
+export default routesConfig;
